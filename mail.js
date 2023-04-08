e373a3bafd70c6a57ecb6c40d09e22c2cb5619f2
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-08 15:57:17 +0800

    added failure.html

--- mail.js
+++ mail.js
@@ -41,7 +41,9 @@ app.post('/', (req, res) => {
   const request = https.request(url, options, (response) => {
 
     if (response.statusCode === 200) {
-      res.send("Successfully subscribed");
+      res.sendFile(__dirname + "/success.html")
+    } else {
+      res.sendFile(__dirname + "/failure.html")
     }
 
     response.on("data", (data) => {
