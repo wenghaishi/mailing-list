e01759215f4088109d5527e2e11201e6a5c9b3fd
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-08 15:28:19 +0800

    created const request

--- mail.js
+++ mail.js
@@ -30,11 +30,18 @@ app.post('/', (req, res) => {
 
   const jsonData = JSON.stringify(data);
 
-  const url = 'https://us9.api.mailchimp.com/3.0/lists/b832680292'
+  const url = 'https://us9.api.mailchimp.com/3.0/lists/b832680292';
 
-  https.request(url, options, () => {
+  const options = {
+    method: "POST",
+    auth: "sam:64af96b9e1ede50c3c6b45ac59b972e8-us9"
+  }
 
-  })
+  const request = https.request(url, options, (response) => {
+    response.on("data", (data) => {
+      console.log(JSON.parse(data));
+    });
+  });
 
 })
 
